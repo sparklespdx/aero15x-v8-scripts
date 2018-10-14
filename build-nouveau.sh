7a15d2e2f18d2499f31878b0baed108767a25255
Josh <josh.farwell@gmail.com>
2018-10-14 14:37:43 -0700

    Added patch file and clean build from Linux kernel tree
    Signed-off-by: Josh <josh.farwell@gmail.com>

--- build-nouveau.sh
+++ build-nouveau.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # Go to kernel source tree and build
-cd /home/josh/git/linux/drivers/gpu/drm/nouveau
+
+sudo echo 'Starting patch and build of Nouveau module...'
+
+orig_dir=$PWD
+cd /home/josh/git/linux
+
+git reset --hard
+git checkout v$(uname -r | cut -d'.' -f1-2)
+patch -p1 < ../aero15x-scripts/nouveau-power.patch
+
+cd drivers/gpu/drm/nouveau
 make -C /lib/modules/$(uname -r)/build M=$(pwd) clean
 make -j6 -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 sudo make -j6 -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install
@@ -9,3 +19,5 @@ make -C /lib/modules/$(uname -r)/build M=$(pwd) clean
 
 # Remove module installed by package
 sudo rm /lib/modules/$(uname -r)/kernel/drivers/gpu/drm/nouveau/nouveau.ko.xz
+cd $orig_dir
+echo 'Patch applied.'
