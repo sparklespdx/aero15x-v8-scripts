831cad52785015ec6d15443a0754398f71280c5d
Josh <josh.farwell@gmail.com>
2018-10-14 17:07:23 -0700

    Added more &&s because they make everything better
    Signed-off-by: Josh <josh.farwell@gmail.com>

--- build-nouveau.sh
+++ build-nouveau.sh
@@ -10,15 +10,16 @@ if [ ! -d ./linux ]; then
 fi
 
 # Reset git tree and apply Karol's patch
-# Only check out releases
+# Only checks out releases, edit this if you need to build 
+# against a release candidate or master.
 cd linux
-git reset --hard
-git fetch
-git checkout v$(uname -r | cut -d'.' -f1-2)
-patch -p1 < ../nouveau-power.patch
+git reset --hard && \
+git fetch && \
+git checkout v$(uname -r | cut -d'.' -f1-2) && \
+patch -p1 < ../nouveau-power.patch && \
 
 # Build nouveau and install module
-cd drivers/gpu/drm/nouveau
+cd drivers/gpu/drm/nouveau && \
 make -C /lib/modules/$(uname -r)/build M=$(pwd) clean && \
 make -j6 -C /lib/modules/$(uname -r)/build M=$(pwd) modules && \
 sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) modules_install && \
